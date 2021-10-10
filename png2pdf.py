f454d0335d82899fc6e13a99bdf4eabc22213893
SSSEAL-C <83465508+SSSEAL-C@users.noreply.github.com>
2021-10-11 00:00:20 +1100

    Update png2pdf.py

--- png2pdf.py
+++ png2pdf.py
@@ -1,24 +1,25 @@
 import os
 from PIL import Image
 from fpdf import FPDF
-from tqdm import tqdm 
+from tqdm import tqdm
 pdf = FPDF()
-w,h = 0,0
-firstfilenumber = 0 #Change this
-lastfilenumber = 0 #Change this
+w, h = 0, 0
+firstfilenumber = 0  # Change this
+lastfilenumber = 0  # Change this
 
 for i in tqdm(range(firstfilenumber, lastfilenumber+1)):
-    fname = "file%d.png" % i #file name here, %d = number, for eg. file%d.png will be file0.png, %.3d = file000.png
+    # file name here, %d = number, for eg. file%d.png will be file0.png, %.3d = file000.png
+    fname = "file%d.png" % i
     if os.path.exists(fname):
         if i == firstfilenumber:
             cover = Image.open(fname)
-            w,h = cover.size
-            pdf = FPDF(unit = "pt", format = [w,h])
+            w, h = cover.size
+            pdf = FPDF(unit="pt", format=[w, h])
         image = fname
         pdf.add_page()
-        pdf.image(image,0,0,w,h)
+        pdf.image(image, 0, 0, w, h)
     else:
        ## print("File not found:", fname)
-    ##print("processed %d" % i)
+        ##print("processed %d" % i)
 pdf.output("output.pdf", "F")
 print("done")
